81c0ddbfe899a9ad16ba935263dad62a0958de53
icefery <icefery@163.com>
2022-04-20 18:27:49 +0800

    [agent] 使用 posix 拼接路径

--- ftp-client-agent/src/modules/ftp/ftp.function.ts
+++ ftp-client-agent/src/modules/ftp/ftp.function.ts
@@ -1,99 +1,14 @@
-import { IFile } from '../fs/fs.interface'
-import { Session } from '../session/session.entity'
 import * as path from 'path'
 
+import { IFile } from '../fs/fs.interface'
+
 import { Client, FileType } from 'basic-ftp'
-
-// export async function ls(session: Session, src: string): Promise<IFile[]> {
-//   const client = new Client()
-//   try {
-//     await client.access({ host: session.host, port: session.port, user: session.user, password: session.pass })
-//     const list = await client.list(src)
-//     return list.reduce<IFile[]>((prev, curr) => {
-//       const filename = curr.name
-//       const filepath = path.join(src, filename)
-//       const filesize = curr.size
-//       const filetype = curr.type === FileType.Directory ? 'd' : curr.type === FileType.File ? '-' : 'unknown'
-//       if (filetype !== 'unknown') {
-//         prev.push({ type: filetype, name: filename, path: filepath, size: filesize })
-//       }
-//       return prev
-//     }, [])
-//   } finally {
-//     client.close()
-//   }
-// }
-
-// export async function mkdir(session: Session, dst: string): Promise<void> {
-//   const client = new Client()
-//   try {
-//     await client.access({ host: session.host, port: session.port, user: session.user, password: session.pass })
-//     await client.ensureDir(dst)
-//   } finally {
-//     client.close()
-//   }
-// }
-
-// export async function mv(session: Session, src: string, dst: string): Promise<void> {
-//   const client = new Client()
-//   try {
-//     await client.access({ host: session.host, port: session.port, user: session.user, password: session.pass })
-//     await client.ensureDir(path.dirname(dst))
-//     await client.rename(dst, src)
-//   } finally {
-//     client.close()
-//   }
-// }
-
-// export async function rm(session: Session, src: string): Promise<void> {
-//   const client = new Client()
-//   try {
-//     await client.access({ host: session.host, port: session.port, user: session.user, password: session.pass })
-//     await client.removeDir(src)
-//   } finally {
-//     client.close()
-//   }
-// }
-
-// export async function get(
-//   session: Session,
-//   src: string,
-//   dst: string,
-//   callback: (total: number, current: number) => void
-// ): Promise<void> {
-//   const client = new Client()
-//   try {
-//     await client.access({ host: session.host, port: session.port, user: session.user, password: session.pass })
-//     client.trackProgress((info) => callback(info.bytesOverall, info.bytes))
-//     await client.downloadTo(src, dst)
-//     client.trackProgress()
-//   } finally {
-//     client.close()
-//   }
-// }
-
-// export async function put(
-//   session: Session,
-//   src: string,
-//   dst: string,
-//   callback: (total: number, current: number) => void
-// ): Promise<void> {
-//   const client = new Client()
-//   try {
-//     await client.access({ host: session.host, port: session.port, user: session.user, password: session.pass })
-//     client.trackProgress((info) => callback(info.bytesOverall, info.bytes))
-//     await client.uploadFrom(src, dst)
-//     client.trackProgress()
-//   } finally {
-//     client.close()
-//   }
-// }
 
 export async function ls(client: Client, src: string): Promise<IFile[]> {
   const list = await client.list(src)
   return list.reduce<IFile[]>((prev, curr) => {
     const filename = curr.name
-    const filepath = path.join(src, filename)
+    const filepath = path.posix.join(src, filename)
     const filesize = curr.size
     const filetype = curr.type === FileType.Directory ? 'd' : curr.type === FileType.File ? '-' : 'unknown'
     if (filetype !== 'unknown') {
@@ -108,7 +23,7 @@ export async function mkdir(client: Client, dst: string): Promise<void> {
 }
 
 export async function mv(client: Client, src: string, dst: string): Promise<void> {
-  await client.ensureDir(path.dirname(dst))
+  await client.ensureDir(path.posix.dirname(dst))
   await client.rename(dst, src)
 }
 

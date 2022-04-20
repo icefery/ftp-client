7ba3938eadcad4c17c2595a6db5a3988a060af1e
icefery <icefery@163.com>
2022-04-21 04:09:41 +0800

    [ui] temp commit

--- ftp-client-ui/vite.config.js
+++ ftp-client-ui/vite.config.js
@@ -2,14 +2,16 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import Components from 'unplugin-vue-components/vite'
-import { NaiveUiResolver, AntDesignVueResolver } from 'unplugin-vue-components/resolvers'
+import { NaiveUiResolver, AntDesignVueResolver, ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+
+import Components from 'unplugin-vue-components/vite'
 
 export default defineConfig({
   plugins: [
     vue(),
     vueJsx(),
     Components({
-      resolvers: [NaiveUiResolver(), AntDesignVueResolver()]
+      resolvers: [NaiveUiResolver(), AntDesignVueResolver(), ElementPlusResolver()]
     })
   ]
 })

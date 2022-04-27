e7f1c7dbd85dc2b9221a599d1c999c3bdc546be7
icefery <icefery@163.com>
2022-04-27 23:32:56 +0800

    [agent] 配置添加注释

--- ftp-client-agent/src/config/index.ts
+++ ftp-client-agent/src/config/index.ts
@@ -1,8 +1,14 @@
 import * as path from 'path'
 import * as os from 'os'
 
+// 用户主目录
 export const HOME_PATH = os.homedir()
 
+// 配置文件目录
 export const CONFIG_PATH = path.resolve(HOME_PATH, '.ftp-client')
 
+// 数据库文件路径
 export const DB_PATH = path.resolve(CONFIG_PATH, 'ftp-client.db')
+
+// FTP 超时时间
+export const FTP_TIMEOUT = 3000
